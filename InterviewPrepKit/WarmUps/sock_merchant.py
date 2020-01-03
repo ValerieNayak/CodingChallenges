7765bf2dfaded2fd0caf62b786c804e6eb61d8ce
Valerie Nayak <valerienayak@gmail.com>
2020-01-03 11:43:01 -0500

    completed sock merchant

--- InterviewPrepKit/WarmUps/sock_merchant.py
+++ InterviewPrepKit/WarmUps/sock_merchant.py
@@ -4,28 +4,21 @@
 
 #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
-
 # Complete the sockMerchant function below.
 def sockMerchant(n, ar):
-    return None
+    # n is int length of input
+    # ar is list of ints
+    socks_dict = {}
+    pair_count = 0
+    for s in ar:
+        if s in socks_dict:
+            socks_dict[s] += 1
+            if socks_dict[s] % 2 == 0:
+                pair_count += 1
+        else:
+            socks_dict[s] = 1
+    return pair_count
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = sockMerchant(n, ar)
-
-    print('n', n)
-    print('ar', ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+total_socks = 9
+socks = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+print(sockMerchant(total_socks, socks))
